8e80f53c33d09b80b212359a3d87499f28a4655c
rtroe-win <rob.tom.roe@gmail.com>
2022-02-06 22:10:40 -0800

    Added launch script to linux builds

---
+++ start-linux.sh
@@ -0,0 +1,4 @@
+# metricracer-server-standalone
+echo "Starting Server for " $HOSTNAME
+./linux-x64/MetricRacerNetServer -n "${HOSTNAME}-1" -p 14242 &
+./linux-x64/MetricRacerNetServer -n "${HOSTNAME}-2" -p 14243 &
